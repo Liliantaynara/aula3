ce751b3784f35892284bb42b4cc8a045316d411b
Lilian <taynaraaluna6@gmail.com>
2025-04-01 14:47:00 -0300

    numero

---
+++ aula3.py
@@ -0,0 +1,12 @@
+numeroum = int (input( "coloque numero: "))
+numerodois = int (input( "coloque numero: "))
+
+soma = numeroum + numerodois
+div = numeroum / numerodois
+mult = numeroum * numerodois
+sub = numeroum - numerodois
+
+print (f" a soma de {numeroum} + {numerodois} = {soma}")
+print (f" a soma de {numeroum} / {numerodois} = {div}")
+print (f" a soma de {numeroum} * {numerodois} = {mult}")
+print (f" a soma de {numeroum} - {numerodois} = {sub}")
