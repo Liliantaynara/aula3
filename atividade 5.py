88ea8972695d03bf4bb2b2126afdb52c2d5c7a57
Lilian <taynaraaluna6@gmail.com>
2025-04-01 15:57:08 -0300

    aiaiaiiaiaiiaiaisahdjasnd

---
+++ atividade 5.py
@@ -0,0 +1,10 @@
+n1 = float (input( "digite nota 1: "))
+n2 = float (input("Digite nota 2: "))
+n3 = float (input("diigite nota3: "))
+
+media = ( n1 + n2 + n3)/3
+
+if media >= 7:
+    print(f"voce foi aprovado {media}" )
+else:
+    print(f"voce foi reprovado {media}" )
