48c04502f2a9bb37617bb67603b809cefacc6774
Lilian <taynaraaluna6@gmail.com>
2025-04-01 16:19:37 -0300

    da 3 notas e da a media e no final diz se ta reprovado ou n

--- atividade 5.py
+++ atividade 5.py
@@ -5,6 +5,13 @@ n3 = float (input("diigite nota3: "))
 media = ( n1 + n2 + n3)/3
 
 if media >= 7:
-    print(f"voce foi aprovado {media}" )
+    print(f"aprovado {media}")
+
 else:
-    print(f"voce foi reprovado {media}" )
+    if media <=4:
+        print(f"recuperacao{media}")
+    else:
+        print(f"vc foi reprovado {media}")
+
+
+
